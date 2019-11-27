1999eca251ad74fa59d142858470e010c71a19a3
Ishita Sequeira <sequeira.i@northeastern.edu>
2019-11-27 18:26:00 -0500

    added vm in main.tf

--- infrastructure/azure/terraform/main.tf
+++ infrastructure/azure/terraform/main.tf
@@ -4,28 +4,96 @@ provider "azurerm" {}
 
 # Create the resource group
 resource "azurerm_resource_group" "ccwebapp" {
-  location = "${var.region}"
+  location = var.region
   name = "ccwebapp_infrastructure"
 }
 
 resource "azurerm_virtual_network" "app_network" {
-  address_space = ["${var.network_address}"]
-  location = "${azurerm_resource_group.ccwebapp.location}"
+  address_space = [var.network_address]
+  location = azurerm_resource_group.ccwebapp.location
   name = "app_virtual_network"
-  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
 
-  subnet {
-    address_prefix = "${var.subnet_addresses[0]}"
-    name = "subnet1"
+  # subnet {
+  #   address_prefix = "${var.subnet_addresses[0]}"
+  #   name = "subnet1"
+  # }
+
+  # subnet {
+  #   address_prefix = "${var.subnet_addresses[1]}"
+  #   name = "subnet2"
+  # }
+
+  # subnet {
+  #   address_prefix = "${var.subnet_addresses[2]}"
+  #   name = "subnet3"
+  # }
+}
+
+resource "azurerm_subnet" "main" {
+  count = 3
+
+  name = "subnet-${count.index}"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefix = var.subnet_addresses[count.index]
+}
+
+
+resource "azurerm_network_interface" "main" {
+  name = "ccwebapp-nic"
+  location = azurerm_resource_group.ccwebapp.location
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+
+  ip_configuration {
+    name = "testconfiguration1"
+    subnet_id = azurerm_subnet.main.0.id
+    private_ip_address_allocation = "Dynamic"
   }
 
-  subnet {
-    address_prefix = "${var.subnet_addresses[1]}"
-    name = "subnet2"
+  depends_on = [azurerm_subnet.main]
+}
+
+data "azurerm_image" "custom" {
+  name_regex                = "csye6225_*"
+  resource_group_name = "myResourceGroup"
+  sort_descending = true
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name = "ccwebapp-vm"
+  location = azurerm_resource_group.ccwebapp.location
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size = "Standard_B1s"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    id = "${data.azurerm_image.custom.id}"
   }
 
-  subnet {
-    address_prefix = "${var.subnet_addresses[2]}"
-    name = "subnet3"
+  storage_os_disk {
+    name = "myosdisk1"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+//    image_uri = data.azurerm_image.custom.id
+  }
+  os_profile {
+    computer_name = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
