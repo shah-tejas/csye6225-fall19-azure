164a46a5189e93f5e61d808d164617403b8de6eb
SumitAnglekar94 <anglekar.s@northeastern.edu>
2019-12-03 03:24:49 -0500

    added autoscaling

--- infrastructure/azure/terraform/main.tf
+++ infrastructure/azure/terraform/main.tf
@@ -212,7 +212,7 @@ resource "azurerm_virtual_machine" "main" {
 
 
 resource "azurerm_postgresql_server" "example" {
-  name                = "ccwebapp-postgresql"
+  name                = "ccwebapppostgresql"
   location            = "${azurerm_resource_group.ccwebapp.location}"
   resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
 
@@ -272,22 +272,21 @@ resource "azurerm_postgresql_firewall_rule" "example" {
 #Load Balancer
 
 #Associate both Vm to the same resource group
-resource "azurerm_resource_group" "load_balancer" {
-  name     = "LoadBalancerRG"
-  location = var.region
-}
-
 resource "azurerm_public_ip" "load_balancer" {
   name                = "PublicIPForLB"
   location            = var.region
-  resource_group_name = "${azurerm_resource_group.load_balancer.name}"
+  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
   allocation_method   = "Static"
+  #domain_name_label   = "${azurerm_resource_group.ccwebapp.name}"
+   tags = {
+    environment = "staging"
+  }
 }
 
 resource "azurerm_lb" "load_balancer" {
   name                = "TestLoadBalancer"
   location            = var.region
-  resource_group_name = "${azurerm_resource_group.load_balancer.name}"
+  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
@@ -296,13 +295,13 @@ resource "azurerm_lb" "load_balancer" {
 }
 
 resource "azurerm_lb_backend_address_pool" "load_balancer" {
-  resource_group_name = "${azurerm_resource_group.load_balancer.name}"
+  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
   loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
   name                = "BackEndAddressPool"
 }
 
 resource "azurerm_lb_rule" "load_balancer" {
-  resource_group_name            = "${azurerm_resource_group.load_balancer.name}"
+  resource_group_name            = "${azurerm_resource_group.ccwebapp.name}"
   loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
   name                           = "LBRule"
   protocol                       = "Tcp"
@@ -311,6 +310,103 @@ resource "azurerm_lb_rule" "load_balancer" {
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 
+resource "azurerm_lb_nat_pool" "load_balancer" {
+  resource_group_name            = "${azurerm_resource_group.ccwebapp.name}"
+  name                           = "ssh"
+  loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_probe" "load_balancer" {
+  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
+  loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
+  name                = "http-probe"
+  protocol            = "Http"
+  request_path        = "/health"
+  port                = 8080
+}
+
+resource "azurerm_virtual_machine_scale_set" "load_balancer" {
+  name                = "mytestscaleset-1"
+  location            = "${azurerm_resource_group.ccwebapp.location}"
+  resource_group_name = "${azurerm_resource_group.ccwebapp.name}"
+
+  # automatic rolling upgrade
+  automatic_os_upgrade = true
+  upgrade_policy_mode  = "Rolling"
+
+  rolling_upgrade_policy {
+    max_batch_instance_percent              = 20
+    max_unhealthy_instance_percent          = 20
+    max_unhealthy_upgraded_instance_percent = 5
+    pause_time_between_batches              = "PT0S"
+  }
+
+  # required when using rolling upgrade policy
+  health_probe_id = "${azurerm_lb_probe.load_balancer.id}"
+
+  sku {
+    name     = "Standard_F2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "testvm"
+    admin_username       = "myadmin"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+        key_data = var.ssh_public_key
+        path = "/home/myadmin/.ssh/authorized_keys"
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "TestIPConfiguration"
+      primary                                = true
+      subnet_id                              = "${azurerm_subnet.dbsub.id}"
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.load_balancer.id}"]
+      load_balancer_inbound_nat_rules_ids    = ["${azurerm_lb_nat_pool.load_balancer.id}"]
+    }
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
 
 
 
