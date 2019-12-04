41aaadfb508e307ba28c6ecc7cee1045edfd3494
Tejas Shah <shah.te@husky.neu.edu>
2019-12-04 02:58:48 -0500

    Implement load balancer and auto scaling

--- infrastructure/azure/terraform/main.tf
+++ infrastructure/azure/terraform/main.tf
@@ -25,80 +25,87 @@ resource "azurerm_subnet" "main" {
 }
 
 
-resource "azurerm_network_interface" "main" {
-  name = "ccwebapp-nic"
-  location = azurerm_resource_group.ccwebapp.location
-  resource_group_name = azurerm_resource_group.ccwebapp.name
-  network_security_group_id = azurerm_network_security_group.network_sg.id
+//resource "azurerm_network_interface" "main" {
+//  name = "ccwebapp-nic"
+//  location = azurerm_resource_group.ccwebapp.location
+//  resource_group_name = azurerm_resource_group.ccwebapp.name
+//  network_security_group_id = azurerm_network_security_group.network_sg.id
+//
+//  ip_configuration {
+//    name = "testconfiguration1"
+//    subnet_id = azurerm_subnet.main.0.id
+//    private_ip_address_allocation = "Dynamic"
+//    public_ip_address_id = azurerm_public_ip.network_public_ip.id
+//  }
+//
+//  depends_on = [azurerm_subnet.main]
+//}
 
-  ip_configuration {
-    name = "testconfiguration1"
-    subnet_id = azurerm_subnet.main.0.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.network_public_ip.id
-  }
+//resource "azurerm_public_ip" "network_public_ip" {
+//  name = "vm_public_ip"
+//  resource_group_name = azurerm_resource_group.ccwebapp.name
+//  location = var.region
+//  allocation_method = "Dynamic"
+//}
 
-  depends_on = [azurerm_subnet.main]
-}
-
-resource "azurerm_public_ip" "network_public_ip" {
+resource "azurerm_public_ip" "network_public_ip2" {
   name = "vm_public_ip"
   resource_group_name = azurerm_resource_group.ccwebapp.name
   location = var.region
   allocation_method = "Dynamic"
 }
 
-resource "azurerm_network_security_group" "network_sg" {
-  name = "network_security_group"
-  resource_group_name = azurerm_resource_group.ccwebapp.name
-  location = var.region
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    access = "Allow"
-    direction = "Inbound"
-    name = "ssh_access"
-    priority = 1001
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "22"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    access = "Allow"
-    direction = "Inbound"
-    name = "tomcat_access"
-    priority = 1000
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "8080"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    access = "Allow"
-    direction = "Outbound"
-    name = "outbound_access"
-    priority = 1000
-    protocol = "*"
-    source_port_range = "*"
-    destination_port_range = "*"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-}
+//resource "azurerm_network_security_group" "network_sg" {
+//  name = "network_security_group"
+//  resource_group_name = azurerm_resource_group.ccwebapp.name
+//  location = var.region
+//
+//  security_rule {
+//    name                       = "HTTP"
+//    priority                   = 100
+//    direction                  = "Inbound"
+//    access                     = "Allow"
+//    protocol                   = "Tcp"
+//    source_port_range          = "*"
+//    destination_port_range     = "80"
+//    source_address_prefix      = "*"
+//    destination_address_prefix = "*"
+//  }
+//
+//  security_rule {
+//    access = "Allow"
+//    direction = "Inbound"
+//    name = "ssh_access"
+//    priority = 1001
+//    protocol = "Tcp"
+//    source_port_range = "*"
+//    destination_port_range = "22"
+//    source_address_prefix = "*"
+//    destination_address_prefix = "*"
+//  }
+//  security_rule {
+//    access = "Allow"
+//    direction = "Inbound"
+//    name = "tomcat_access"
+//    priority = 1000
+//    protocol = "Tcp"
+//    source_port_range = "*"
+//    destination_port_range = "8080"
+//    source_address_prefix = "*"
+//    destination_address_prefix = "*"
+//  }
+//  security_rule {
+//    access = "Allow"
+//    direction = "Outbound"
+//    name = "outbound_access"
+//    priority = 1000
+//    protocol = "*"
+//    source_port_range = "*"
+//    destination_port_range = "*"
+//    source_address_prefix = "*"
+//    destination_address_prefix = "*"
+//  }
+//}
 
 data "azurerm_image" "custom" {
   name_regex                = "csye6225_*"
@@ -106,48 +113,185 @@ data "azurerm_image" "custom" {
   sort_descending = true
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name = "ccwebapp-vm"
+//resource "azurerm_virtual_machine" "main" {
+//  name = "ccwebapp-vm"
+//  location = var.region
+//  resource_group_name = azurerm_resource_group.ccwebapp.name
+//  network_interface_ids = [azurerm_network_interface.main.id]
+//  vm_size = "Standard_B1s"
+//
+//  # Uncomment this line to delete the OS disk automatically when deleting the VM
+//  delete_os_disk_on_termination = true
+//
+//  # Uncomment this line to delete the data disks automatically when deleting the VM
+//  delete_data_disks_on_termination = true
+//
+//  storage_image_reference {
+//    id = "${data.azurerm_image.custom.id}"
+//  }
+//
+//  storage_os_disk {
+//    name = "myosdisk1"
+//    caching = "ReadWrite"
+//    create_option = "FromImage"
+//    managed_disk_type = "Standard_LRS"
+//  }
+//  os_profile {
+//    computer_name = "hostname"
+//    admin_username = "testadmin"
+//    custom_data = templatefile("${path.module}/prepare_azure_vm.sh", {
+//        azure_db_endpoint = "jdbc:postgresql://${azurerm_postgresql_server.example.name}.postgres.database.azure.com:5432/${var.db_username}@${azurerm_postgresql_server.example.name}",
+//        azure_db_username = "${var.db_username}@${azurerm_postgresql_server.example.name}",
+//        azure_db_password = "${var.db_password}"
+//      })
+//  }
+//  os_profile_linux_config {
+//    disable_password_authentication = true
+//    ssh_keys {
+//      key_data = var.ssh_public_key
+//      path = "/home/testadmin/.ssh/authorized_keys"
+//    }
+//  }
+//
+//  tags = {
+//    environment = "staging"
+//  }
+//}
+
+# LOAD BALANCER
+resource "azurerm_lb" "vm-loadbalancer" {
   location = var.region
+  name = "ccwebapp-loadbalancer"
   resource_group_name = azurerm_resource_group.ccwebapp.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size = "Standard_B1s"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    id = "${data.azurerm_image.custom.id}"
+  frontend_ip_configuration {
+    name = "PublicIPForLB"
+    public_ip_address_id = azurerm_public_ip.network_public_ip2.id
   }
+}
 
-  storage_os_disk {
-    name = "myosdisk1"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name = "hostname"
-    admin_username = "testadmin"
-    custom_data = templatefile("${path.module}/prepare_azure_vm.sh", {
-        azure_db_endpoint = "jdbc:postgresql://${azurerm_postgresql_server.example.name}.postgres.database.azure.com:5432/${var.db_username}@${azurerm_postgresql_server.example.name}",
-        azure_db_username = "${var.db_username}@${azurerm_postgresql_server.example.name}",
-        azure_db_password = "${var.db_password}"
-      })
-  }
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      key_data = var.ssh_public_key
-      path = "/home/testadmin/.ssh/authorized_keys"
+resource "azurerm_lb_backend_address_pool" "lb-backend" {
+  loadbalancer_id = azurerm_lb.vm-loadbalancer.id
+  name = "BackEnd-for-lb"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+}
+
+resource "azurerm_lb_probe" "vm-loadbalancer-probe" {
+  loadbalancer_id = azurerm_lb.vm-loadbalancer.id
+  name = "vm-probe"
+  port = 8080
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+}
+
+resource "azurerm_lb_rule" "lb-rule" {
+  backend_port = 8080
+  frontend_ip_configuration_name = azurerm_lb.vm-loadbalancer.frontend_ip_configuration[0].name
+  frontend_port = 8080
+  loadbalancer_id = azurerm_lb.vm-loadbalancer.id
+  name = "http"
+  protocol = "Tcp"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+}
+
+resource "azurerm_virtual_machine_scale_set" "vm-autoscale" {
+  location = var.region
+  name = "VM-Autoscale"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+  upgrade_policy_mode = "Manual"
+
+  network_profile {
+    name = "terraformnetworkprofile"
+    primary = true
+    ip_configuration {
+      name = "IPConfiguration"
+      primary = true
+      subnet_id = azurerm_subnet.main.0.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.lb-backend.id]
     }
   }
+  os_profile {
+    admin_username = "testadmin"
+    admin_password = "CSYE6225-test"
+    computer_name_prefix = "vmlab"
+    custom_data = templatefile("${path.module}/prepare_azure_vm.sh", {
+      azure_db_endpoint = "jdbc:postgresql://${azurerm_postgresql_server.example.name}.postgres.database.azure.com:5432/${var.db_username}@${azurerm_postgresql_server.example.name}",
+      azure_db_username = "${var.db_username}@${azurerm_postgresql_server.example.name}",
+      azure_db_password = "${var.db_password}"
+    })
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  sku {
+    capacity = 3
+    name = "Standard_DS1_v2"
+  }
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  storage_profile_data_disk {
+    lun          = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+  storage_profile_image_reference {
+    id = "${data.azurerm_image.custom.id}"
+  }
+}
 
-  tags = {
-    environment = "staging"
+resource "azurerm_monitor_autoscale_setting" "vm-autoscale-setting" {
+  location = var.region
+  name = "AutoscalingForVM"
+  resource_group_name = azurerm_resource_group.ccwebapp.name
+  target_resource_id = azurerm_virtual_machine_scale_set.vm-autoscale.id
+  enabled = true
+  profile {
+    name = "Always"
+    capacity {
+      default = 3
+      maximum = 10
+      minimum = 3
+    }
+    rule {
+      metric_trigger {
+        metric_name = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.vm-autoscale.id
+        operator = "GreaterThan"
+        statistic = "Average"
+        threshold = 5
+        time_aggregation = "Average"
+        time_grain = "PT1M"
+        time_window = "PT5M"
+      }
+      scale_action {
+        cooldown = "PT10M"
+        direction = "Increase"
+        type = "ChangeCount"
+        value = 1
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.vm-autoscale.id
+        operator = "LessThan"
+        statistic = "Average"
+        threshold = 3
+        time_aggregation = "Average"
+        time_grain = "PT1M"
+        time_window = "PT5M"
+      }
+      scale_action {
+        cooldown = "PT10M"
+        direction = "Decrease"
+        type = "ChangeCount"
+        value = 1
+      }
+    }
   }
 }
 
